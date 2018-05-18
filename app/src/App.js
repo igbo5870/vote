26c90d7b06e0674cf83c1e6e6e5434016137709f
Grégory <gregory.bouloc@gmail.com>
2018-05-18 16:59:56 +0200

    ajouter du design

--- app/src/App.js
+++ app/src/App.js
@@ -1,35 +1,58 @@
 import React, { Component } from 'react';
 
-import ListName from './ListName'
-import menu_close from './menu_close.svg';
-import list from './list.json';
+import ListName from './Component/ListName';
+import menu_close from './Image/menu_close.svg';
+import list from './Data/list.json';
 import './App.css';
-//import MyForm from './handleEventVote';
+
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: null
+      name: null,
+      countOUI: 0,
+      countNON: 0
     }
 
-    this.publish = this.publish.bind(this);
+    this.onClickOUI = this.onClickOUI.bind(this);
+    this.onClickNon = this.onClickNon.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
  
-  // Traitement lors du clic sur le bouton oui 
-  publish() {
+  onClickNon(){
+    var inputValue = this.state.nameInput
+    var listName = <ListName list={list} />
+    
+    for(var i = 0; i < list.length; i++){
+      if(inputValue === listName.props.list[i]['first_name']){
+        this.setState({
+          countNON: this.state.countNON + 1
+        });
+        document.getElementById(i).classList.remove('no-vote')
+        document.getElementById(i).classList.add('vote');
+      }
+    }
+    
+  }
+
+  onClickOUI(){
     var inputValue = this.state.nameInput
     var listName = <ListName list={list} />
 
     for(var i = 0; i < list.length; i++){
-      if(inputValue === listName.props.list[i]['first_name']){
-        document.getElementById(i).classList.toggle('vote');
+      var className = document.getElementById(i).classList.contains('no-vote')
+
+      if(inputValue === listName.props.list[i]['first_name'] && className){
+        document.getElementById(i).classList.remove('no-vote')
+        document.getElementById(i).classList.add('vote');
+        this.setState({
+          countOUI: this.state.countOUI + 1
+        });
       }
     }
-    alert(inputValue)
- }
+  }
  // Change la valeur sur l'input text
   handleChange({ target }) {
     this.setState({
@@ -47,38 +70,42 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <p className="jeVote">JE VOTE!</p>
           <img src={menu_close} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bienvenue sur l'application de vote</h1>
+          
         </header>
-        <div className="message">
-          <p className="pmessage">Application en cours de développement.</p>
+        <div className="content-page">
+          <div className ="content-middle">
+            <div className="question">
+            <h1>{this.state.inputQuestion}</h1>
+            </div>
+            <div className="form-vote"> 
+              <form onSubmit={doSomething}>
+                <label htmlFor="nameInput">Moi, </label>
+                <input 
+                    id="nameInput" class="nameInput"name="nameInput" type="text" value={ this.state.nameInput }
+                    onChange={ this.handleChange } 
+                />
+                <label> je vote</label><br/>
+                <button id="btOui" class="btOui" onClick={ this.onClickOUI }>OUI</button>
+                <button id="btNon" class="btNon" onClick={ this.onClickNon } >NON</button>
+              </form>
+            </div>
+          </div>
+          <div className="slide-right">
+            <div className="form-question">
+              <p>Votre question :</p>
+              <input type="text" id="inputQuestion" class="inputQuestion" placeholder="Posez votre question ici" name="inputQuestion" onChange={ this.handleChange }  />
+            </div>
+            <div className="listname">
+              <div><ListName list={list} /></div>
+              <p>OUI : {this.state.countOUI} </p>
+              <p>NON : {this.state.countNON} </p>
+            </div>
         </div>
-        <div className="listname">
-          Votre Liste :
-          <ListName 
-            list={list}
-          />
-        </div>
-        <div className='form-vote'> 
-          <form onSubmit={(doSomething)}>
-              <label htmlFor="nameInput">Moi, </label>
-              <input 
-                  id="nameInput" name="nameInput" type="text" value={ this.state.nameInput }
-                  onChange={ this.handleChange } 
-              />
-              <label> je vote</label><br/>
-              <button  onClick={ this.publish } >Oui</button>
-              <button id='unCheck'>Non</button>
-              
-            </form>
         </div>
       </div>
-      
-    );
-
-    
-    
-    
+    ); 
   }
 }
 
--- app/src/Component/ListName.js
+++ app/src/Component/ListName.js
@@ -4,9 +4,10 @@
  export default function ListName(props){
     return (
         <ul className="nameList">
+            Votre liste :
             {
                  props.list.map((row) => (
-                 <li id={row.id} className='no-vote'key={row.id}>{row.first_name}</li>  
+                 <li id={row.id} className='no-vote' key={row.id}>{row.first_name}</li>  
                 ))
             }
         </ul>
